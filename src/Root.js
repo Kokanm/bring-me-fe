26cb1b20980bf82b6c41cce2d592d15c97d91240
andrius.kucinskas02 <andrius.kucinskas@geniussports.com>
2019-07-24 15:39:48 +0200

    Connect to GraphQL server + flow

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,40 @@
+// @flow
+
 import React from 'react';
 import { Text } from 'react-native';
+import { QueryRenderer, graphql } from 'react-relay';
+import environment from './relay/environment';
+import type { RootQueryResponse } from './__generated__/RootQuery.graphql';
+
+type Props = {
+  +error: {
+    +message: String,
+  },
+  +props: RootQueryResponse,
+};
+
+const renderInner = ({ error, props }: Props) => {
+  if (error) {
+    return <Text>{error.message}</Text>;
+  } else if (props) {
+    return <Text>IT works</Text>;
+  }
+  return <Text>Loading</Text>;
+};
 
 export default function Root() {
-  return <Text>Root</Text>;
+  return (
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+        query RootQuery {
+          users {
+            id
+            firstName
+          }
+        }
+      `}
+      render={renderInner}
+    />
+  );
 }
