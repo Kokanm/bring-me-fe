ad3a2ddbb212f474728b853229bd4bce709f121b
Roman Horodianeko <necto68necto68@gmail.com>
2019-07-25 16:55:51 +0200

    feat: start details screen

--- src/navigation/screens/deliveryDetails/DeliveryDetails.js
+++ src/navigation/screens/deliveryDetails/DeliveryDetails.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import styled from 'styled-components/native';
 import { Text } from 'native-base';
+import { withNavigation } from 'react-navigation';
 
-export default function OrderDetails() {
+const Root = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ItemImage = styled.Image`
+  width: 300;
+  height: 300;
+`;
+
+const AcceptDeliveryButton = styled.Button`
+  align-self: center;
+`;
+
+function OrderDetails({ navigation }) {
+  const delivery = navigation.getParam('delivery');
+
   return (
-    <View>
-      <Text>Order details</Text>
-    </View>
+    <Root>
+      <Text>{`${delivery.item}`}</Text>
+      <ItemImage source={{ uri: delivery.imageURL }} />
+      <Text>{`From: ${delivery.locationFrom}`}</Text>
+      <Text>{`To: ${delivery.locationTo}`}</Text>
+      <AcceptDeliveryButton title="Accept delivery" />
+    </Root>
   );
 }
+
+export default withNavigation(OrderDetails);
