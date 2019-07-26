0c5cf5378061aead2ccebafdd4d4e7d0cfdeb4b0
andrius.kucinskas02 <andrius.kucinskas@geniussports.com>
2019-07-26 09:55:58 +0200

    Change server link

--- src/relay/environment.js
+++ src/relay/environment.js
@@ -2,7 +2,7 @@ import { Environment, Network, RecordSource, Store } from 'relay-runtime';
 
 function fetchQuery(operation, variables) {
   // TODO change to proper one
-  return fetch('http://399453f8.ngrok.io/graphql', {
+  return fetch('http://17a4ea50.ngrok.io/graphql', {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
